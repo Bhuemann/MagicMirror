80dd5eb993b7ed0ef8c25a3a6b166808e4739db5
Randy <rpitcher@purdue.edu>
2016-02-28 20:43:11 -0500

    Established database / DBtoServer Communication
    Created a test database and have started CRUD operations from the node
    server to the mongodb database!!!

---
+++ MongoDB-Test/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://127.0.0.1:27017/accounting', function(err, connection) {
+	var collection = connection.collection('customers');
+
+	collection.insert({'name': 'Jane Doe'}, function(err, count) {
+		collection.find().toArray(function(err, documents) {
+			console.dir(documents);
+			connection.close();
+		});//end toArray
+	});//end insert
+});//end connect
