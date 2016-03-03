320017c80641b79c2bab4c07ab3a6ba51fa04740
Randy <rpitcher@purdue.edu>
2016-03-03 13:24:49 -0500

    implemented ajax requests for logging in

--- frontEnd/js/login.js
+++ frontEnd/js/login.js
@@ -1,11 +1,23 @@
-$(document).ready(function(){
-	$("loginform cf").function validateForm()
-	{
-		var x=document.forms["login"]["username"].value;
-		if (x==null || x=="")
-		  {
-		  alert("Please fill out the username");
-		  return false;
-		  }
-	}
-}
+$(document).ready(function() {
+$('#login-form').submit(function(e) {
+        // Prevent form submission
+        e.preventDefault();
+
+        // Get the form instance
+        var $form = $(e.target);
+
+        // Get the BootstrapValidator instance
+        var bv = $form.data('bootstrapValidator');
+
+        // Use Ajax to submit form data
+         $.ajax({
+            type: 'get',
+            url: '/login',
+            data: $form.serialize(),
+            success: function (data) {
+              alert('form was submitted');
+              console.log(data);
+            }
+          });
+    });
+});
