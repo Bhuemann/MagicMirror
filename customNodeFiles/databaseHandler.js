5dab753657c21cd8403db30dc05d4a9f968f4a1d
Randy <rpitcher@purdue.edu>
2016-03-03 16:29:57 -0500

    The backend should be working now

--- customNodeFiles/databaseHandler.js
+++ customNodeFiles/databaseHandler.js
@@ -55,54 +55,48 @@ function login(query, res) {
 	console.log("Password: " + password);
 
 	MongoClient.connect('mongodb://127.0.0.1:27017/users', function(err, connection) {
-	var collection = connection.collection('users');
+		var collection = connection.collection('users');
 
-	collection.find({'username': username}).toArray(function (err, documents) {
-		console.dir(documents);
-		callback();
-	});
-	
+		var userEntry = collection.find({'username': username}).toArray(function (err, documents) {
+			console.dir(documents);
+			//res.send("login valid");
 
-	var doInsert = function (i) {
-		if (i < 20) {
-			var value = Math.floor(Math.random() * 10);
-			collection.insert(
-				{'n': '#' + i, 'v': value},
-				function (err, count) {
-					doInsert(i + 1);
-				});
-		} else {
-			console.log();
-			console.log('Inserted', i, 'documents:');
-			doFind(function() {
-				doUpdate();
-			});
-		}
-	};
+			//res.send("wrong password");
 
-	var doUpdate = function() {
-		collection.update(
-		{'username': {'$gt': 5}},
-		{'$set': {'valuable': true}},
-		{'multi': true},
-		function(err, count) {
-			console.log();
-			console.log('Updated', count, 'documents:');
-			doFind(function() {
-				collection.remove({}, function () {
-					connection.close();
-				});
-			});
-		});//end of update
-	};
+			//res.send("no user found");
+		});
 
-	doInsert(0);
-});//end connect
-
+	});//end connect
 }
 
-function newUser(req) {
-	return "not yet implemented";
+function newUser(req, res) {
+	var username = query.username;
+	var password = query.password;
+
+	console.log("New Username: " + username);
+	console.log("New Password: " + password);
+
+	MongoClient.connect('mongodb://127.0.0.1:27017/users', function(err, connection) {
+		var collection = connection.collection('users');
+
+		collection.find({'username': username}).toArray(function (err, documents) {
+			console.dir(documents);
+			if (documents === []) {
+				insertNewUser(username, password);
+			} else {
+				res.send("username already exists");
+				console.log("This goddamn username already exists! Tell the client to fuckoff");
+			}
+		});
+
+		var insertNewUser = function (username, password) {
+			collection.insert({'username': username, 'password': password},
+				function (err, count) {
+					res('user added');
+					console.log("new user added!");
+				});
+		};
+	});//end connect*/
 }
 
 exports.login = login;
