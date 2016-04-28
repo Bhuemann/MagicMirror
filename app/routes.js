c586f86c11bf6fbfe39168d32858ad01d3d7a902
Randy <rpitcher@purdue.edu>
2016-04-28 15:40:18 -0400

    added session control for local accounts

---
+++ app/routes.js
@@ -0,0 +1,80 @@
+var User = require('../app/models/user');
+
+module.exports = function(app, passport) {
+	//homepage
+	app.get('/', isLoggedIn, function(req, res) {
+		//if logged in, send to profile page, otherwise isLoggedIn will handle it
+		res.redirect('/home');
+	});
+
+	//login
+	app.get('/login', function(req, res) {
+		//load login page with any flash data if it exists
+		res.render('../frontEnd/login.ejs', { message: req.flash('loginMessage') });
+	});
+	app.get('/login.html', function(req, res) {
+		//load login page with any flash data if it exists
+		res.redirect('/login');
+	});
+
+	//process login form
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/home',
+		failureRedirect : '/login',
+		failureFlash : true
+	}));
+
+	//signup
+	app.get('/createAccount', function(req, res) {
+		//load signup page with any flash data if it exists
+		res.render('../frontEnd/createAccount.ejs', { message: req.flash('signupMessage') });
+	});
+	app.get('/createAccount.html', function(req, res) {
+		//load login page with any flash data if it exists
+		res.redirect('/createAccount');
+	});
+
+	//process signup form
+	app.post('/createAccount', passport.authenticate('local-signup', {
+		successRedirect : '/home',
+		failureRedirect : '/createAccount',
+		failureFlash : true
+	}));
+
+	//go to profile (make sure they're logged in)
+	app.get('/home', isLoggedIn, function(req, res) {
+		res.render('../frontEnd/index.ejs');
+	});
+	app.get('/index.html', function(req, res) {
+		//load login page with any flash data if it exists
+		res.redirect('/home');
+	});
+
+	//go to profile (make sure they're logged in)
+	app.get('/visualizations', isLoggedIn, function(req, res) {
+		res.render('../frontEnd/visualizationsIndex.ejs');
+	});
+	//go to profile (make sure they're logged in)
+	app.get('/visualizationsIndex.html', isLoggedIn, function(req, res) {
+		res.redirect('/visualizations');
+	});
+
+	//logout
+	app.get('/logout', function(req, res) {
+		req.user.activeGame = false;
+		req.user.save(function(err) {
+			req.logout();
+			res.redirect('/');
+		});		
+	});
+
+	//function for checking user is logged in
+	function isLoggedIn(req, res, next) {
+		if (req.isAuthenticated()) {
+			return next();
+		}
+
+		//if not authenticated, redirect to default page
+		res.render('../frontEnd/login.ejs');//load default page
+	}
+};
--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 'use strict';
 
-var express = require('express');
-var ourDB = require("./customNodeFiles/databaseHandler")
+//require core external libraries
+var express = require('express')
 var app = express();
+var http = require('http').Server(app);
+var mongoose = require('mongoose');
 var passport = require('passport');
+var flash = require('connect-flash');
 
-//Configure google authentication dependencies
-var strategy = require('./customNodeFiles/passport.js');
-strategy.gStrategy(passport);
+//require helper libraries
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
+//database config file
+var configDB = require('./config/database.js');
 
-app.get('/', function(req,res) {
-	console.log("Recieved initial connection.")
-	res.sendFile(__dirname+'/frontEnd/login.html');
-});
+//setup database
+mongoose.connect(configDB.url);//connect to our DB
 
-app.get('/login', function(req,res) {
-	console.log("login request received");
-	ourDB.login(req.query, res);
-});
+//configure passport
+require('./config/passport')(passport);
 
-app.get('/newUser', function(req,res) {
-	console.log("newUser request received");
-	ourDB.newUser(req.query, res);
-});
-
-app.get('/google-login', passport.authenticate('google', { scope : ['profile', 'email']}));
-
-
+//setup express
+app.use(morgan('dev')); //log every request to the console
+app.use(cookieParser()); //read cookies
+app.use(bodyParser()); //read html forms
 app.use(express.static('frontEnd'));
+app.set('view engine', 'ejs'); //setup ejs for templating
+
+//setup passport
+app.use(session({ secret: 'RavenclawAllDay'}));//session secret
+app.use(passport.initialize());
+app.use(passport.session()); //persistent login sessions
+app.use(flash()); //use for flash messages
+
+//setup routes
+require('./app/routes.js')(app, passport);
 
 app.listen(8888, function() {
 	console.log("Express listening on port 8888");
