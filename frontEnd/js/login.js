e95e4b6f7909d907242d7ae01b83e484da1e70dd
Randy <rpitcher@purdue.edu>
2016-04-28 16:27:39 -0400

    fixed the login page too

---
+++ frontEnd/js/login.js
@@ -0,0 +1,19 @@
+var availableTags = ["ActionScript", "AppleScript", "Asp", "BASIC", "C", "C++", "Clojure",
+                  "COBOL", "ColdFusion", "Erlang", "Fortran", "Groovy", "Haskell", "Java", "JavaScript",
+                  "Lisp", "Perl", "PHP", "Python", "Ruby", "Scala", "Scheme" ];
+
+$(document).ready(function() {
+  $('.form-control').keyboard({ layout: 'qwerty' })
+  .autocomplete({
+    source: availableTags
+  })
+  // position options added after v1.23.4
+  .addAutocomplete({
+    position : {
+      of : null,        // when null, element will default to kb.$keyboard
+      my : 'right top', // 'center top', (position under keyboard)
+      at : 'left top',  // 'center bottom',
+      collision: 'flip'
+    }
+  }).addTyping();
+})
